884d050cdc65f1c387f1aaf72f1ed5f8774cdb37
Hasan Hazırbulan <115214634+hasanhazirbulan@users.noreply.github.com>
2024-05-01 19:22:04 +0300

    Create InvertBinary.java

---
+++ InvertBinary.java
@@ -0,0 +1,43 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // Constructing a sample binary tree
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode inverted = solution.invertTree(root);
+
+        // Print the inverted tree (assuming TreeNode has a suitable toString() method)
+        System.out.println(inverted);
+    }
+}
