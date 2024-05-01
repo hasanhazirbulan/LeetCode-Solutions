f0edead695b52263b9bb5b24412a0162a4968923
Hasan Hazırbulan <115214634+hasanhazirbulan@users.noreply.github.com>
2024-05-01 16:34:01 +0300

    Add files via upload

---
+++ BaseballGame.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(op));
+            }
+        }
+
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
+
---
+++ implementStackUsingQueues.java
@@ -0,0 +1,80 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+    private int top;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q1.offer(x);
+        top = x;
+    }
+
+    public int pop() {
+        while (q1.size() > 1) {
+            top = q1.poll();
+            q2.offer(top);
+        }
+        int popValue = q1.poll();
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        return popValue;
+    }
+
+    public int top() {
+        return top;
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+
+
+
+
+/* Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+
+
+Example 1:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, top, and empty.
+All the calls to pop and top are valid.
+*/
